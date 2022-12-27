886b53172ca8d945ef516df24836e7c79fb172fe
yahyabakkar <yahyabakkar022@gmail.com>
2022-12-27 11:55:29 +0200

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const { restart } = require('nodemon')
 const app = express()
 const port = 3000
 
-app.get(/\//, (req, res) => {
-  res.send('Ok')
+app.get ( "/" ,(req, res) => {
+  res.send('Ok.') ;
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 }) 
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
+
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage')
+})
+app.get("/test", (req, res) => {
+  res.status(200).send({ status: 200, message: "ok" });
+}) 	;
+
+app.get("/time", (req, res) => {
+  let time = new Date();
+  res
+    .status(200)
+    .send({ status: 200, message: `${time.getHours()}:${time.getMinutes()}` });
+});
+
+app.listen(port, console.log(`Connected to ${port}`));  
